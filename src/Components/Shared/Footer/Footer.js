d1a7e2b2d4009dbe4c0aa78be8328fe92bd648bf
unknown <montasirazad@gmail.com>
2022-09-02 03:57:03 +0600

    social media icon added in footer

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,28 +1,127 @@
 import React from 'react';
 import './Footer.css';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GoogleIcon from '@mui/icons-material/Google';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 const Footer = () => {
     return (
-        <div className='footer-div text-center '>
-            <div className="row">
-                <div className='col-lg-3 footer-card'>
-                    <h6>Discover</h6>
+        <div >
+
+            <footer className="text-center text-lg-start bg-white text-muted">
+
+                <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+
+                    <div className="me-5 d-none d-lg-block">
+                        <span>Get connected with us on social networks:</span>
+                    </div>
+
+                    <div>
+                        <a href="" className="me-4 link-secondary">
+                            < FacebookIcon sx={{ fontSize: 40 }} color="primary"/>
+                        </a>
+                        <a href="" className="me-4 link-secondary">
+                            <InstagramIcon sx={{ fontSize: 40 }} />
+                        </a>
+                        <a href="" className="me-4 link-secondary">
+                            <TwitterIcon sx={{ fontSize: 40 }} />
+                        </a>
+                        <a href="" className="me-4 link-secondary">
+                            <GoogleIcon sx={{ fontSize: 40 }} />
+                        </a>
+                        <a href="" className="me-4 link-secondary">
+                            <LinkedInIcon sx={{ fontSize: 40 }} />
+                        </a>
+                        <a href="" className="me-4 link-secondary">
+                            <GitHubIcon sx={{ fontSize: 40 }} />
+                        </a>
+                    </div>
+
+                </section>
+
+                <section className="">
+                    <div className="container text-center text-md-start mt-5">
+
+                        <div className="row mt-3">
+
+                            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    <i className="fas fa-gem me-3 text-secondary"></i>Company name
+                                </h6>
+                                <p>
+                                    Here you can use rows and columns to organize your footer content. Lorem ipsum
+                                    dolor sit amet, consectetur adipisicing elit.
+                                </p>
+                            </div>
+
+                            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    Products
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Angular</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">React</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Vue</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Laravel</a>
+                                </p>
+                            </div>
+
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    Useful links
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Pricing</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Settings</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Orders</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Help</a>
+                                </p>
+                            </div>
+
+                            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+                                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                                <p><i className="fas fa-home me-3 text-secondary"></i> New York, NY 10012, US</p>
+                                <p>
+                                    <i className="fas fa-envelope me-3 text-secondary"></i>
+                                    info@example.com
+                                </p>
+                                <p><i className="fas fa-phone me-3 text-secondary"></i> + 01 234 567 88</p>
+                                <p><i className="fas fa-print me-3 text-secondary"></i> + 01 234 567 89</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </section>
+
+
+
+                <div className="text-center p-4" style={{ backgroundColor: ' #45637d', color: 'white' }}>
+                    © {new Date().getFullYear()} dvantagepoint
+
                 </div>
 
-                <div className='col-lg-3 footer-card'>
-                    <h6>Learn About</h6>
-                </div>
+            </footer>
 
-                <div className='col-lg-3 footer-card'>
-                    <h6>Connect With Us</h6>
-                </div>
-
-                <div className='col-lg-3'>
-                    <h6>About </h6>
-                </div>
-            </div>
-            <div>
-                <p> {new Date().getFullYear()} </p>
-            </div>
         </div>
     );
 };
