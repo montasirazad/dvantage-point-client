53bc5c6e7d8a83d5f0100a54f9d3a7e1e95167e3
unknown <montasirazad@gmail.com>
2022-09-02 16:47:00 +0600

    added yarn react hash link

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -17,13 +17,13 @@ const Footer = () => {
 
             <footer className="text-center text-lg-start bg-white text-dark border-top mt-5">
 
-                <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+                <section className="d-flex justify-content-center  p-4 border-bottom">
 
-                    <div className="me-5 d-none d-lg-block">
+                    {/* <div className="me-5 d-none d-lg-block">
                         <span>Get connected with us on social networks:</span>
-                    </div>
+                    </div> */}
 
-                    <div>
+                    <div  >
                         <a href="" className="me-4 link-secondary">
                             < FacebookIcon sx={{ fontSize: 40 }} color="primary" />
                         </a>
