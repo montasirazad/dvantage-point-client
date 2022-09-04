3d509810714f756f2ed6456e117f451230514443
unknown <montasirazad@gmail.com>
2022-09-04 17:41:44 +0600

    added new video in slider

--- src/Components/Shared/Slider/Slider.js
+++ src/Components/Shared/Slider/Slider.js
@@ -3,6 +3,7 @@ import pic1 from '../../../images/pic-1.jpg';
 import pic2 from '../../../images/pic-2.png';
 import pic3 from '../../../images/pic-3.jpg';
 import circuitGif from '../../../images/circuit-mesh.mp4';
+import bgGif from '../../../images/bg-gif.gif'
 
 const Slider = () => {
     return (
@@ -21,15 +22,24 @@ const Slider = () => {
 
                         {/* <img src={pic1} className="d-block w-100" alt="..." /> */}
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>First slide label</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
+                            <h5></h5>
+                            <p>dvantgaepoint is an information technology and services company providing
+                                solution engineering and consultancy services to clients from Telecommunications,
+                                Fintech, Riders and Online media verticals. We are small but focused group of
+                                globally connected diversified and experienced IT & software professionals from
+                                Canada, Germany, China and Bangladesh. </p>
                         </div>
                     </div>
                     <div className="carousel-item">
-                        <img src={pic2} className="d-block w-100" alt="..." />
+                        <img src={bgGif} className="d-block w-100" alt="..." />
                         <div className="carousel-caption d-none d-md-block">
                             <h5>Second slide label</h5>
-                            <p>Some representative placeholder content for the second slide.</p>
+                            <p>We intend to belong in production readiness tollgates of our clients
+                                for business-critical applications and software starting from design board until
+                                it sees production lights.
+                                Let us focus on operational excellence and reliability
+                                engineering and assist in fostering your photons
+                            </p>
                         </div>
                     </div>
 
