be42511baf823a442ec1b5d4ae47370d9a1d6b3d
unknown <montasirazad@gmail.com>
2022-09-08 03:17:27 +0600

    working on bg video

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
                 <Container fluid>
                     <Navbar.Brand href="#" className='mx-5 logo-title'
                         >
-                            <img style={{width:'240px' }}  src={dvpLogo} alt="" />
+                            <img style={{width:'180px' }}  src={dvpLogo} alt="" />
                         </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
@@ -28,8 +28,8 @@ const Header = () => {
                         >
                             <Nav.Link href="#action1" className='header mx-3'  >HOME</Nav.Link>
                             <Nav.Link as={HashLink} to='#about' className='header mx-3'>ABOUT</Nav.Link>
-                            <NavDropdown title="PRODUCT AND SERVICES" className='header' id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action3" className='header '>Augmented Capacity Planning and Benchmarking </NavDropdown.Item>
+                            <NavDropdown title="PRODUCT & SERVICES" className='header' id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action3" className='header '>Augmented Capacity Planning & Benchmarking </NavDropdown.Item>
                                 <NavDropdown.Item href="#action4" className='header'>Cloud Engineering </NavDropdown.Item>
                                 {/* <NavDropdown.Divider /> */}
                                 <NavDropdown.Item href="#action5" className='header'>fsMon </NavDropdown.Item>
@@ -38,7 +38,7 @@ const Header = () => {
                                 <NavDropdown.Item href="#action8" className='header '>next item 1</NavDropdown.Item>
                                 <NavDropdown.Item href="#action9" className='header'>next item 2</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link as={HashLink} to='#blog' className='header mx-5'>BLOG</Nav.Link>
+                            <Nav.Link as={HashLink} to='#blog' className='header mx-3'>BLOG</Nav.Link>
                         </Nav>
 
                     </Navbar.Collapse>
--- src/Components/Shared/Slider/Slider.js
+++ src/Components/Shared/Slider/Slider.js
@@ -1,68 +1,25 @@
 import React from 'react';
-import pic1 from '../../../images/pic-1.jpg';
-import pic2 from '../../../images/pic-2.png';
-import pic3 from '../../../images/pic-3.jpg';
 import circuitGif from '../../../images/circuit-mesh.mp4';
-import bgGif from '../../../images/bg-gif.gif'
+import './Slider.css'
+
 
 const Slider = () => {
     return (
-        <div>
-            <div id="carouselExampleCaptions" className="carousel slide col-sm-12 col-md-12" data-bs-ride="carousel">
-                <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
+        <div className=' d-flex flex-row'>
 
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <video src={circuitGif} className="d-block w-100"
-                            autoPlay muted loop type="video/mp4"></video>
-
-                        {/* <img src={pic1} className="d-block w-100" alt="..." /> */}
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5></h5>
-                            <p >dvantgaepoint is an information technology and services company providing
-                                solution engineering and consultancy services to clients from Telecommunications,
-                                Fintech, Riders and Online media verticals. We are small but focused group of
-                                globally connected diversified and experienced IT & software professionals from
-                                Canada, Germany, China and Bangladesh. </p>
-                        </div>
-                    </div>
-
-                    {/* <div className="carousel-item">
-                        <img src={bgGif} className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Second slide label</h5>
-                            <p>We intend to belong in production readiness tollgates of our clients
-                                for business-critical applications and software starting from design board until
-                                it sees production lights.
-                                Let us focus on operational excellence and reliability
-                                engineering and assist in fostering your photons
-                            </p>
-                        </div>
-                    </div> */}
-
-                    {/* <div className="carousel-item">
-                        <img src={pic3} className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Third slide label</h5>
-                            <p>Some representative placeholder content for the third slide.</p>
-                        </div>
-                    </div> */}
-
-                </div>
-
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
+            <div className=' text-center' style={{ backgroundColor: 'black', border: '1px solid blue', width: '700px', height: '400px' }}>
+                <h1 className='text-white'>hello</h1>
             </div>
+
+            <div className=''>
+                {/* <video src={circuitGif} style={{ width: '700px' }}
+                    autoPlay muted loop type="video/mp4"></video> */}
+                {/* style={{ border: '1px solid blue', width: '700px', height: '400px' }} */}
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" autoPlay muted loop src={circuitGif}  ></iframe>
+                </div>
+            </div>
+
         </div>
     );
 };
