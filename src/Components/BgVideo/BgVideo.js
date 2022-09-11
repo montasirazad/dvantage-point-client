6e555b2cabb236169a5f2afbcc71d7e7cc0c3e67
unknown <montasirazad@gmail.com>
2022-09-11 16:17:19 +0600

    updated landing page

--- src/Components/BgVideo/BgVideo.js
+++ src/Components/BgVideo/BgVideo.js
@@ -3,13 +3,14 @@ import './BgVideo.css';
 import bgVideo from '../../images/circuit-mesh.mp4';
 import dvpLogo from '../../images/dvp_logo.png'
 import Typewriter from 'typewriter-effect';
+import bgImage from '../../images/cuircuitBg2.jpg';
 
 const BgVideo = () => {
 
 
     return (
         <div id='home-div'>
-            
+            <img className='bg-img' src={bgImage} alt="" />
 
             <div className='font'>
                 <Typewriter
@@ -21,13 +22,15 @@ const BgVideo = () => {
                 />
             </div>
 
-            <Typewriter
-                options={{
-                    strings: ['Lorem ipsum dolor sit amet'],
-                    autoStart: true,
-                    loop: true,
-                }}
-            />
+            <div className='font-2'>
+                <Typewriter
+                    options={{
+                        strings: ['Lorem ipsum dolor sit amet'],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                />
+            </div>
             <p></p>
         </div>
     );
