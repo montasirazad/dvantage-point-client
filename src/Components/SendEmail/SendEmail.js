b582ba47b43a7ccdf5289b15cdd90505e8da4ef8
unknown <montasirazad@gmail.com>
2022-09-29 16:15:57 +0600

    edited email service

--- src/Components/SendEmail/SendEmail.js
+++ src/Components/SendEmail/SendEmail.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-
 
 const SendEmail = () => {
 
-    const sendEmail = (e) => {
+    const form = useRef();
 
-        emailjs.sendForm('service_j9mejet', 'template_tuojvqv', e.target, '2KKH7XFFU2ztXq5yM')
+    const sendMail = (e) => {
+        e.preventDefault();
+
+        // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+
+        emailjs.sendForm('service_l2oqthc', 'template_g8x4kte', form.current, 'eol-8LbPGsExreCdv')
             .then((result) => {
                 console.log(result.text);
                 alert('Your message has been sent successfully')
@@ -14,22 +18,21 @@ const SendEmail = () => {
             }, (error) => {
                 console.log(error.text);
             });
-        e.preventDefault()
-    }
+
+    };
+
 
     return (
         <div className='row'>
-
-
             <div className='col-xs-2 col-sm-2 col-lg-2'></div>
             <div className='col-xs-8 col-sm-8 col-lg-8 '>
                 <div className='my-4 p-4 container-fluid'>
 
-                    <form className=" g-3 m-0 auto" onSubmit={sendEmail}>
+                    <form className=" g-3 m-0 auto" ref={form} onSubmit={sendMail}>
 
                         <div >
                             <label htmlFor="inputEmail4" className="form-label">Your name</label>
-                            <input type="text" name='name' className="form-control" placeholder="Your name" />
+                            <input type="text" name='user_name' className="form-control" placeholder="Your name" />
                         </div>
 
                         <div >
@@ -46,6 +49,8 @@ const SendEmail = () => {
                             <button type="submit" className="btn btn-primary">SUBMIT</button>
                         </div>
                     </form>
+
+
                 </div>
             </div>
             <div className='col-xs-2 col-sm-2 col-lg-2'></div>
